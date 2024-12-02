06f352d183fe896b75894d8abefa4e8fea35c573
regisd1023 <regisd@gmail.com>
2024-12-02 18:02:40 -0500

    base made.

--- 2023/D4-1.py
+++ 2023/D4-1.py
@@ -1,7 +1,8 @@
 # template for advent 2023
 
 debug=True
-#debug=False
+debug=False
+My_File = 'D4-data.txt'
 
 inputData = []
 def load_data():
@@ -19,6 +20,8 @@ def cardsanitize(mytext):
     card_lists = []
     winning = []
     mine = []
+    if debug:
+        print(mytext)
     trim1 = mytext.split(':')[1]
     trim2 = trim1.split('|')
     winstring = trim2[0].split()
@@ -26,27 +29,39 @@ def cardsanitize(mytext):
     for x in winstring:
         winning.append(int(x))
     for y in minestring:
-        mine.append(int(x))
+        mine.append(int(y))
     card_lists = [winning, mine]
+    if debug:
+        print(card_lists)
     return(card_lists)
 
 
-
 def check_card(myline):
+    cardvalue = 0
     cardsections = []
     cardsections = cardsanitize(myline)
-
-
-
-
-
-
+    for mynum in cardsections[0]:
+        if debug:
+            print(mynum)
+        if mynum in cardsections[1]:
+            if cardvalue == 0:
+                cardvalue = 1
+            else:
+                cardvalue = cardvalue * 2
+    return(cardvalue)
 
 
 
 def main():
+    rawcards = []
     load_data()
-
+    card_total = 0
+    for card in inputData:
+        print(card)
+        x=check_card(card)
+        print(x)
+        card_total = card_total + x
+    print('total ',card_total)
 
 if __name__ == "__main__":
     main()
